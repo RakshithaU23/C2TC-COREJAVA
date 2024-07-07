4f2281535a182bf8d0570cab7e0cbbd0a13cc807
Rakshitha <Rakshitha@Hrithik>
2024-07-07 21:14:59 +0530

    first commit

---
+++ Rakshithaa/src/inheritance/hierarchicalInheritance.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+class Animal{
+}
+class Tiger extends Animal{	
+}
+class monkey extends Animal{	
+}
+public class hierarchicalInheritance {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+
---
+++ Rakshithaa/src/inheritance/singleInheritance.java
@@ -0,0 +1,24 @@
+package inheritance;
+class Animal{
+	void eat() {
+	}
+}
+class Dog extends Animal{
+	void bark() {
+	}
+}
+public class singleInheritance {
+public static void main(String args[]) {
+		
+		Animal a=new Animal();
+		a.eat();//can access only animal
+		
+		Dog d= new Dog();
+		d.eat();
+		d.bark();//can access both animal and dog methods
+		
+		Animal a1=new Dog();// can access only animal method
+		a1.eat();
+		
+	}
+}
